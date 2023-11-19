db9dc1aa825dfbf90ecf1a20680ebdd1b09a6e33
professorbaptista <josbaptista28@yahoo.com.br>
2023-11-19 00:41:52 +0000

    mais

--- javascript.js
+++ javascript.js
@@ -4,3 +4,9 @@ function carregar(){
     var cor =document.getElementById('corpo')
     cor.style.backgroundColor = 'black'
 }
+
+function text (){
+    var texto = document.getElementById('text')
+    texto.innerText = 'Veja Bem-Vindo'
+
+}
